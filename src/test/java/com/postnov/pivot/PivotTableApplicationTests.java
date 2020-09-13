6e774e841bb689675d869d4e3cd9345e464fb078
Anatoliy Postnov <postnov-90@mail.ru>
2020-09-13 16:11:46 +0300

    Pivot table

--- src/test/java/com/postnov/pivot/PivotTableApplicationTests.java
+++ src/test/java/com/postnov/pivot/PivotTableApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PivotTableApplicationTests {
+class PivotApplicationTests {
 
 	@Test
 	void contextLoads() {
